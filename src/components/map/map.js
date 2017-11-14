bfa8b9938f5c5f2bc5aa84bb506cd1b093300c1a
Terry Lu Zijian <terryluzijian@gmail.com>
2017-11-15 01:56:21 +0800

    Added Responsive Map

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -6,10 +6,11 @@ dotenv.config();
 mapboxgl.accessToken = process.env.REACT_APP_MapboxAccessToken;
 
 class Map extends Component {
+  
   componentDidMount() {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
+      style: 'mapbox://styles/mapbox/light-v9',
       zoom: 16,
       center: [103.776587, 1.296644],
       maxBounds: [
@@ -17,6 +18,18 @@ class Map extends Component {
           [103.786345, 1.310327]
       ]
     });
+    var navControl = new mapboxgl.NavigationControl();
+    this.map.addControl(navControl, 'bottom-right');
+
+    var popup = new mapboxgl.Popup({closeOnClick: false, closeButton: false})
+    .setLngLat([103.776587, 1.296644])
+    .setHTML('<div><h3>Bus</h3><p style="padding: 5%">3min</p><div>')
+    .addTo(this.map);
+
+    var popup = new mapboxgl.Popup({closeOnClick: false, closeButton: false})
+    .setLngLat([103.774587, 1.295644])
+    .setHTML('<h3>Taxi</h3>')
+    .addTo(this.map);
   }
 
   componentWillUnmount() {
@@ -24,13 +37,9 @@ class Map extends Component {
   }
 
   render() {
-    const style = {
-      top: 0,
-      bottom: 0,
-      width: '100%',
-      height: '800px'
-    };
-    return <div style={style} ref={el => this.mapContainer = el} />;
+    return (
+      <div className="map-container" ref={el => this.mapContainer = el} />
+    );
   }
 }
 
