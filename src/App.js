a8780ba6b43a29ea81fa11b2a92ef56a367aa92d
Terry Lu Zijian <terryluzijian@gmail.com>
2017-11-17 18:31:13 +0800

    Added Customization Chart Theme

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ import Transport from './components/page/transport';
 import './asset/css/mapbox.min.css';
 import './asset/css/default.min.css';
 
+import Highcharts from 'highcharts';
+
+Highcharts.theme = {
+  colors: ['#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+};
+Highcharts.setOptions(Highcharts.theme);
+
 class App extends Component {
   render() {
     return (
